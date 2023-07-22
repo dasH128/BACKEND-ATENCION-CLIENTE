57c208023a8136e64c8a239c5015d77d5e7d7c0d
Dash <rojas71421@gmail.com>
2023-07-22 14:03:40 +0100

    fix

--- src/app/entity/Worker.ts
+++ src/app/entity/Worker.ts
@@ -5,8 +5,8 @@ import { IWorker } from './interface/IWorker'
 var workerSchema = new Schema({
     nombre: { type: String, required: true },
     apellido: { type: String, required: true },
-    dni: { type: String, required: true },
-    correo: { type: String, required: true },
+    dni: { type: String, required: true, unique: true },
+    correo: { type: String, required: true, unique: true },
     isla: { type: String, required: false },
 })
 
--- src/app/repository/WorkerRepository.ts
+++ src/app/repository/WorkerRepository.ts
@@ -11,14 +11,22 @@ export class WorkerRepository {
 
     registrar = async (entity: IWorker) => {
         try {
+            console.log(entity)
             const worker = new Worker(entity)
+            var dniExist = await Worker.find({ dni: entity.dni });
+            console.log('worker dni--->', dniExist);
+            if (dniExist.length > 0) return 'dni existente';
+            var correoExist = await Worker.find({ correo: entity.correo });
+            console.log('worker correo--->', correoExist);
+            if (correoExist.length > 0) return 'correo existente';
+
             var f = await worker.save()
             console.log('f es', f)
-        } catch (error) {
+        } catch (error: any) {
             console.log(WorkerRepository.LOG, error)
-            return false
+            return error.toString()
         }
-        return true
+        return ''
     }
     actualizar = async (entity: IWorker) => false
     eliminar = async (id: ObjectId) => false
